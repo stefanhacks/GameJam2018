eb883a5e6c5c3c1cdd05e927f66ca11b694e06cb
GabrielNied <sekienied@yahoo.com.br>
2018-04-21 03:03:09 -0300

    Mecânica básica > jogador.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public int velocidade;
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		rb.velocity = new Vector2(moveHorizontal * velocidade, 0);
+	}
+}
