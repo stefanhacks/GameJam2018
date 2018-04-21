160553f0cf451f2a3d22e184f55405d5af34093f
GabrielNied <sekienied@yahoo.com.br>
2018-04-21 14:14:03 -0300

    Menu e Créditos

---
+++ Assets/Menu/ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void NewGameBtn(string NewGameLevel)
+	{
+		SceneManager.LoadScene (NewGameLevel);
+	}
+
+	public void ExitGameBtn()
+	{
+		Application.Quit ();
+	}
+}
