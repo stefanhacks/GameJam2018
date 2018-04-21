6dee66d293933af88df9b4db25c773b68cc47ae7
GabrielNied <sekienied@yahoo.com.br>
2018-04-21 08:06:00 -0300

    Camera, Plataforma e Players um, dois, input

--- Assets/Scripts/MainCameraController.cs
+++ Assets/Scripts/MainCameraController.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class MainCameraController : MonoBehaviour {
 
+	public GameObject[] player;
+
+	public float transicao, transicao2;
 	// Use this for initialization
 	void Start () {
-		
+		player = GameObject.FindGameObjectsWithTag ("Player");	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (0, -2 * Time.deltaTime, 0);
+		
+		Vector2 distancia = new Vector2 (0, player[0].transform.position.y - player[1].transform.position.y);
+
+		Debug.Log ("distancia: " + distancia);
+		transicao = -2 * Time.deltaTime;
+		transicao2 = -4 * Time.deltaTime;
+
+		if (distancia.y > -2 && distancia.y < 2) {
+			transform.Translate (0, transicao, 0);
+		} else {
+			transform.Translate (0, transicao2, 0);
+		}
+
+
+		/*
+		if (player [1].transform.position.y <= transform.position.y) {
+			transform.Translate (0, player [1].transform.position.y * Time.deltaTime / 10, 0);
+		} 
+
+		if (player [0].transform.position.y <= transform.position.y) {
+			transform.Translate (0, player [0].transform.position.y * Time.deltaTime / 10, 0);
+		} 
+		*/
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,27 @@ public class PlayerController : MonoBehaviour {
 
 	public int velocidade;
 	Rigidbody2D rb;
+
+	public enum PlayerType { PlayerOne, PlayerTwo };
+	public PlayerType currentPlayer = PlayerType.PlayerOne;
+
+	private float moveHorizontal; 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+
 	}
-	
+
+	void Update(){
+		if (currentPlayer == PlayerType.PlayerOne) {
+			moveHorizontal = Input.GetAxis ("HorizontalP1");
+		} else {
+			moveHorizontal = Input.GetAxis ("HorizontalP2");
+		}
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () {
-		float moveHorizontal = Input.GetAxis("Horizontal");
 		rb.velocity = new Vector2(moveHorizontal * velocidade, 0);
 	}
 }
