4171a56cb23e8b012a8676e35df28bef62e77e7f
GabrielNied <sekienied@yahoo.com.br>
2018-04-21 08:19:25 -0300

    Quick Fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    private GameObject player1, player2;
+    private GameObject[] player;
 
 	void Start () {
 
-        player1 = GameObject.FindGameObjectWithTag("Player1");
-        //player2 = GameObject.FindGameObjectWithTag("Player2");
+        player = GameObject.FindGameObjectsWithTag("Player");
 
     }
 	
 
 	void Update () {
-        Morrer(player1);
-        //Morrer(player2);
+        Morrer(player[0]);
+        Morrer(player[1]);
     }
 
     void Morrer (GameObject play)
     {
-        if (Blitzkrieg.GetGameObjectPosition(player1).y <= -0.3 || Blitzkrieg.GetGameObjectPosition(player1).y > 1.3) {
+		if (Blitzkrieg.GetGameObjectPosition(play).y <= -0.025 || Blitzkrieg.GetGameObjectPosition(play).y > 1.025) {
             Destroy(play);
         }
     }
